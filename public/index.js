0d170b2697ed6d92590b62a60957f9d95a3f45b9
nworb9 <emma.nworb@gmail.com>
2022-10-30 23:37:54 -0400

    added more pictures and some content

--- public/index.js
+++ public/index.js
@@ -4,10 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function (e) {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
       this.classList.toggle("active");
       var panel = this.nextElementSibling;
       if (panel.style.display === "block") {
         panel.style.display = "none";
+      }
+      if (panel.style.display !== "none") {
+        panel.style.display = "block";
+        e.stopPropagation();
+      }
+      if (this.className.contains(" active")) {
+        panel.style.display = "block";
       } else {
         panel.style.display = "block";
         e.stopPropagation();
